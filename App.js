78d3758e67d3b1368403bbabf632039543faaece
SANTOS Cyntia <cyntisan@oberthurcs.com>
2019-04-18 15:57:39 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,27 @@
+var express = require("express"),
+  app = express(),
+  http = require("http").Server(app).listen(80),
+  upload = require("express-fileupload");
+
+app.use(upload());
+
+console.log("Server started!");
+
+app.get("/",function(req, res){
+    res.sendFile(__dirname+"/index.html");
+})
+
+app.post("/",function(req,res){
+  if(req.files){
+    var file = req.files.filename,
+        filename = file.name;
+    file.mv("./upload/"+filename, function(err){
+      if(err){
+        console.log(err);
+        res.send("error ocurred")
+      }else{
+        res.send("Done!")
+      }
+    })
+  }
+})
